9d75861fc71f36985e2564fd57928470dd1437ee
Yury <permenko@gmail.com>
2016-08-13 13:32:34 +0300

    fix show\hide keyboard methods

--- app/src/main/java/com/permenko/weather/view/dialogs/AddCityDialog.java
+++ app/src/main/java/com/permenko/weather/view/dialogs/AddCityDialog.java
@@ -4,8 +4,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -32,7 +36,6 @@ public class AddCityDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        showKeyboard();
         return new AlertDialog.Builder(getContext())
                 .setView(R.layout.dialog_add_city)
                 .setTitle(R.string.add_city)
@@ -48,16 +51,25 @@ public class AddCityDialog extends DialogFragment {
                 .create();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        showKeyboard();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    // should call only when dialog created
     private void showKeyboard() {
-        /*((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE))
-                .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);*/
+        android.app.Dialog dialog = getDialog();
+        if (dialog != null) dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     private void hideKeyboard() {
-        /*((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE))
-                .hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);*/
-
-        //getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
     public interface ClickListener {
