c267dda616c90f7278fd7eb3ad2c01580d6602da
Yury <permenko@gmail.com>
2017-08-08 20:15:43 +0300

    change initial cities

--- app/src/main/java/com/permenko/weather/util/Utils.java
+++ app/src/main/java/com/permenko/weather/util/Utils.java
@@ -19,19 +19,14 @@ public class Utils {
     //hardcoded list of cities for non empty first launch
     public static List<Integer> getInitialData() {
         List<Integer> initialData = new ArrayList<>();
-        initialData.add(232422);
-        initialData.add(2653265);
-        initialData.add(524901);
-        initialData.add(498677);
-        initialData.add(563464);
-        initialData.add(487846);
-        initialData.add(3098030);
-        initialData.add(658225);
-        initialData.add(2759145);
-        initialData.add(588409);
-        initialData.add(3580477);
-        initialData.add(2643743);
-        initialData.add(551487);
+        initialData.add(524901); //moscow
+        initialData.add(2950159); //berlin
+        initialData.add(658225); //helsinki
+        initialData.add(2643743); //london
+        initialData.add(625144); //minsk
+        initialData.add(588409); //tallinn
+        initialData.add(593116); //vilnius
+        initialData.add(456172); //riga
         return initialData;
     }
 
